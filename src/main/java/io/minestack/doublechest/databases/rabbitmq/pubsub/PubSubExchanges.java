b0a9eeced93ae5a866b369f6f6efbf628f0da85f
Jodie Belgrave <rmb1993@gmail.com>
2014-12-25 20:56:28 -0500

    add known queue and exchange names

---
+++ src/main/java/io/minestack/doublechest/databases/rabbitmq/pubsub/PubSubExchanges.java
@@ -0,0 +1,10 @@
+package io.minestack.doublechest.databases.rabbitmq.pubsub;
+
+public enum PubSubExchanges {
+
+    SERVER_START,
+    SERVER_STOP,
+    BUNGEE_START,
+    BUNGEE_STOP
+
+}
---
+++ src/main/java/io/minestack/doublechest/databases/rabbitmq/worker/WorkerQueues.java
@@ -0,0 +1,8 @@
+package io.minestack.doublechest.databases.rabbitmq.worker;
+
+public enum WorkerQueues {
+
+    BUNGEE_BUILD,
+    SERVER_BUILD
+
+}
